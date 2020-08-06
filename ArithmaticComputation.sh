34566c456f4293956e9ba68f2c48ae6f6685cc8a
seema777 <navaghareseema3@gmail.com>
2020-08-07 00:21:34 +1300

    Arthimatic Computation with Array

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -62,3 +62,25 @@ dict["Result4"]=$(( (a % b) + c ))
 echo ${dict[@]}
 
 
+#!/bin/bash -x
+
+read -p "Enter First number: " a
+read -p "Enter Second number: " b
+read -p "Enter Third number: " c
+
+declare -A dict
+
+dict["Result1"]=$(( (a + b) * c ))
+dict["Result2"]=$(( (a * b) + c ))
+dict["Result3"]=$(( (c + a) / b ))
+dict["Result4"]=$(( (a % b) + c ))
+
+echo ${dict[@]}
+
+array[0]=${dict[Result1]}
+array[1]=${dict[Result2]}
+array[2]=${dict[Result3]}
+array[3]=${dict[Result4]}
+
+echo ${array[@]}
+
