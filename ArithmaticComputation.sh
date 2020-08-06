1439957de0ac9899c7c39dbb3b43433d0c4d5476
seema777 <navaghareseema3@gmail.com>
2020-08-07 00:05:40 +1300

    Arthimatic Computation 2

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -9,3 +9,12 @@ read -p "Enter First number: " a
 read -p "Enter Second number: " b
 read -p "Enter Third number: " c
 
+
+#!/bin/bash -x
+
+read -p "Enter First number: " a
+read -p "Enter Second number: " b
+read -p "Enter Third number: " c
+
+echo $(( (a + b) * c ))
+
